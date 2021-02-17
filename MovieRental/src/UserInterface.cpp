82980484a1d435fa4382f54865e9e6bcebdbad82
MG <mg-git.plumbic@8shield.net>
2021-02-17 16:00:34 +0800

    Finished Display Menu Commands

--- MovieRental/src/UserInterface.cpp
+++ MovieRental/src/UserInterface.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void UserInterface::start () {
+
 	displayMenuCommands ();
 }
 
@@ -11,4 +12,16 @@ void UserInterface::displayMenuCommands () {
 	cout << "========================================" << '\n';
 	cout << "      DAVE AND FRIENDS VIDEO STORE      " << '\n';
 	cout << "========================================" << '\n';
+	cout << "[1] New Video" << '\n';
+	cout << "[2] Rent a Video" << '\n';
+	cout << "[3] Return a Video" << '\n';
+	cout << "[4] Show Video Details" << '\n';
+	cout << "[5] Display All Videos" << '\n';
+	cout << "[6] Check Video Availability" << '\n';
+	cout << "[7] Customer Maintenance" << '\n';
+	cout << "[8] Exit Program" << '\n';
+}
+
+void UserInterface::getUserCommand () {
+
 }
