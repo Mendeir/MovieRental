ab001b7c1216421de12c36b4c48df0fbf07d0ba2
MG <mg-git.plumbic@8shield.net>
2021-02-17 16:01:45 +0800

    Fixed Brackets

--- MovieRental/src/UserInterface.cpp
+++ MovieRental/src/UserInterface.cpp
@@ -3,12 +3,14 @@
 
 using namespace std;
 
-void UserInterface::start () {
+void UserInterface::start () 
+{
 
 	displayMenuCommands ();
 }
 
-void UserInterface::displayMenuCommands () {
+void UserInterface::displayMenuCommands () 
+{
 	cout << "========================================" << '\n';
 	cout << "      DAVE AND FRIENDS VIDEO STORE      " << '\n';
 	cout << "========================================" << '\n';
@@ -22,6 +24,7 @@ void UserInterface::displayMenuCommands () {
 	cout << "[8] Exit Program" << '\n';
 }
 
-void UserInterface::getUserCommand () {
+void UserInterface::getUserCommand () 
+{
 
 }
